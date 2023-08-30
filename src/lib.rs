23888d1b31ea46cf0d0dfe9ad59a7b24dc857aac
Daniel Franco <dfranco123@hotmail.com>
2023-08-29 21:08:21 -0500

    better organize modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
-mod search;
-
-pub use search::search::binary_search;
+pub mod searching;
--- src/searching/binary_search.rs
+++ src/searching/binary_search.rs
@@ -1,23 +1,21 @@
-pub mod search {
-    pub fn binary_search<T: PartialEq + PartialOrd>(lst: Vec<T>, item: T) -> Option<usize> {
-        let mut low = 0;
-        let mut high = lst.len() - 1;
+pub fn binary_search<T: PartialEq + PartialOrd>(lst: Vec<T>, item: T) -> Option<usize> {
+    let mut low = 0;
+    let mut high = lst.len() - 1;
 
-        while low <= high {
-            let mid = (low + high) / 2;
-            let estimated_item = &lst[mid];
+    while low <= high {
+        let mid = (low + high) / 2;
+        let estimated_item = &lst[mid];
 
-            if *estimated_item == item {
-                return Some(mid);
-            } else if *estimated_item > item {
-                high = mid - 1
-            } else {
-                low = mid + 1
-            }
+        if *estimated_item == item {
+            return Some(mid);
+        } else if *estimated_item > item {
+            high = mid - 1
+        } else {
+            low = mid + 1
         }
-
-        None
     }
+
+    None
 }
 
 #[cfg(test)]
@@ -30,7 +28,7 @@ mod tests {
             2, 4, 5, 12, 15, 30, 32, 33, 34, 40, 45, 51, 55, 57, 60, 66, 70, 71, 90, 99, 100,
         ];
 
-        assert_eq!(search::binary_search(num_slice, 70), Some(16));
+        assert_eq!(binary_search(num_slice, 70), Some(16));
     }
 
     #[test]
@@ -39,7 +37,7 @@ mod tests {
             2, 4, 5, 12, 15, 30, 32, 33, 34, 40, 45, 51, 55, 57, 60, 66, 70, 71, 90, 99, 100,
         ];
 
-        assert_eq!(search::binary_search(num_slice, 5), Some(2));
+        assert_eq!(binary_search(num_slice, 5), Some(2));
     }
 
     #[test]
@@ -49,13 +47,13 @@ mod tests {
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ];
 
-        assert_eq!(search::binary_search(char_slice, 'l'), Some(11));
+        assert_eq!(binary_search(char_slice, 'l'), Some(11));
     }
 
     #[test]
     fn returns_none_for_chars() {
         let char_slice = vec!['a', 'b', 'c'];
 
-        assert_eq!(search::binary_search(char_slice, 'l'), None);
+        assert_eq!(binary_search(char_slice, 'l'), None);
     }
 }
